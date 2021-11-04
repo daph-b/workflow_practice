e4e34cbaf1babf30ca1f5aa97b1155faffd1106a
daph-b <92843539+daph-b@users.noreply.github.com>
2021-11-04 08:08:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,4 +28,12 @@ resource "google_compute_instance" "default" {
 
 resource "google_compute_network" "vpc_network" {
   name = "vpc-network"
+  auto_create_subnets = "false"
+}
+
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network
 }
