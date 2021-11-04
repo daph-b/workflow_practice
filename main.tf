ef1075ecbd69e91cfc46508e450b08107d27c2e4
daph-b <92843539+daph-b@users.noreply.github.com>
2021-11-04 08:19:37 +0100

    edited the subnet block

--- main.tf
+++ main.tf
@@ -39,5 +39,5 @@ resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" 
   name          = "test-subnetwork"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
-  network       = google_compute_network.vpc_network
+  network       = google_compute_network.vpc_network.name
 }
